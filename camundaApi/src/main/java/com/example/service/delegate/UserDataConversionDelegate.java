f8197629d1c3e255b8050414882890b94aa02e87
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-09-16 18:26:22 +0200

    userDataDone

--- camundaApi/src/main/java/com/example/service/delegate/UserDataConversionDelegate.java
+++ camundaApi/src/main/java/com/example/service/delegate/UserDataConversionDelegate.java
@@ -10,20 +10,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 public class UserDataConversionDelegate implements JavaDelegate {
 
-    @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        // Recupera JSON dalla variabile di processo
-        String json = (String) execution.getVariable("user_info");
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    // Recupera JSON dalla variabile di processo
+    String json = (String) execution.getVariable("user_info");
 
-        // Conversione JSON in oggetto UserData
-        ObjectMapper mapper = new ObjectMapper();
-        UserData userData = mapper.readValue(json, UserData.class);
+    // Conversione JSON in oggetto UserData
+    ObjectMapper mapper = new ObjectMapper();
+    UserData userData = mapper.readValue(json, UserData.class);
 
-        // Salvataggio come variabile di processo
-        execution.setVariable("userDataObj", userData);
+    // Salvataggio come variabile di processo
+    execution.setVariable("user_info", userData);
 
-        // Debug log
-        System.out.println("=== USER DATA OBJECT ===");
-        System.out.println(userData);
-    }
+    // Debug log
+    System.out.println("=== USER DATA OBJECT ===");
+    System.out.println(userData);
+  }
 }
